72c7a67f2e7306184296ec947a58d190f75cacc7
Nelson9594 <boda_nelson@live.fr>
2022-02-18 17:58:23 +0100

    Init

---
+++ log/admin/dashboard/dist/index.php
@@ -0,0 +1,317 @@
+<?php
+	// Initialiser la session
+	session_start();
+	// Vérifiez si l'utilisateur est connecté, sinon redirigez-le vers la page de connexion
+	if(!isset($_SESSION["username"])){
+		header("Location: ../connexion.php");
+		exit(); 
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administrateur</title>
+    
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    
+<link rel="stylesheet" href="assets/vendors/iconly/bold.css">
+
+    <link rel="stylesheet" href="assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
+    <link rel="stylesheet" href="assets/vendors/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/css/app.css">
+    <link rel="shortcut icon" href="assets/images/favicon.svg" type="image/x-icon">
+</head>
+
+<body>
+    <div id="app">
+        <div id="sidebar" class="active">
+            <div class="sidebar-wrapper active">
+    <div class="sidebar-header">
+        <div class="d-flex justify-content-between">
+            <div class="logo">
+                <a href="index.html"><?php echo $_SESSION['username']; ?></a>
+            </div>
+            <div class="toggler">
+                <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
+            </div>
+        </div>
+    </div>
+
+    <!-- debut de la sidebar -->
+
+
+    <?php include 'sidebar.php'; ?>
+
+    <!-- Fin de la sidebar -->
+
+
+    <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+</div>
+        </div>
+        <div id="main">
+            <header class="mb-3">
+                <a href="#" class="burger-btn d-block d-xl-none">
+                    <i class="bi bi-justify fs-3"></i>
+                </a>
+            </header>
+            
+<div class="page-heading">
+    <h3>Global Data</h3>
+</div>
+<div class="page-content">
+    <section class="row">
+        <div class="col-12 col-lg-9">
+            <div class="row">
+                <div class="col-6 col-lg-3 col-md-6">
+                    <div class="card">
+                        <div class="card-body px-3 py-4-5">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="stats-icon purple">
+                                        <i class="iconly-boldShow"></i>
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <h6 class="text-muted font-semibold">Visiteurs</h6>
+                                    <h6 class="font-extrabold mb-0">112.000</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-3 col-md-6">
+                    <div class="card">
+                        <div class="card-body px-3 py-4-5">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="stats-icon blue">
+                                        <i class="iconly-boldProfile"></i>
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <h6 class="text-muted font-semibold">Connexion</h6>
+                                    <h6 class="font-extrabold mb-0">183.000</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-3 col-md-6">
+                    <div class="card">
+                        <div class="card-body px-3 py-4-5">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="stats-icon green">
+                                        <i class="iconly-boldAdd-User"></i>
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <h6 class="text-muted font-semibold">Inscription</h6>
+                                    <h6 class="font-extrabold mb-0">80.000</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-3 col-md-6">
+                    <div class="card">
+                        <div class="card-body px-3 py-4-5">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="stats-icon red">
+                                        <i class="iconly-boldBookmark"></i>
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <h6 class="text-muted font-semibold">Professionnel</h6>
+                                    <h6 class="font-extrabold mb-0">112</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-3 col-md-6">
+                    <div class="card">
+                        <div class="card-body px-3 py-4-5">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="stats-icon red">
+                                        <i class="iconly-boldBookmark"></i>
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <h6 class="text-muted font-semibold">Demande d'hebergement</h6>
+                                    <h6 class="font-extrabold mb-0">112</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-3 col-md-6">
+                    <div class="card">
+                        <div class="card-body px-3 py-4-5">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="stats-icon red">
+                                        <i class="iconly-boldBookmark"></i>
+                                    </div>
+                                </div>
+                                <div class="col-md-8">
+                                    <h6 class="text-muted font-semibold">Demande de conseil</h6>
+                                    <h6 class="font-extrabold mb-0">112</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                
+                
+
+                <div class="col-12 col-xl-8">
+
+                <!-- Message Portfolio -->    
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Message PortFolio</h4>
+                        </div>
+
+                        <div class="card-body">
+                            <div class="table-responsive">
+
+                                <table class="table table-hover table-lg">
+                                    <thead>
+                                        <tr>
+                                            <th>Nom</th>
+                                            <th>Email</th>
+                                            <th>Message</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+                                        <tr>
+                                            <td class="col-3">
+                                                <div class="d-flex align-items-center">
+                                                    <div class="avatar avatar-md">
+                                                        <img src="assets/images/faces/5.jpg">
+                                                    </div>
+                                                    <p class="font-bold ms-3 mb-0">Nelson</p>
+                                                </div>
+                                            </td>
+
+                                            <td class="col-auto">
+                                                <p class=" mb-0">nelson@nelson</p>
+                                            </td>
+
+                                            <td class="col-auto">
+                                                <p class=" mb-0">Excellent Frero</p>
+                                            </td>
+
+                                        </tr>
+                                        
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Message Portfolio -->
+
+
+                    <!-- Message login -->
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Message Connexion</h4>
+                        </div>
+
+                        <div class="card-body">
+                            <div class="table-responsive">
+
+                                <table class="table table-hover table-lg">
+                                    <thead>
+                                        <tr>
+                                            <th>Nom</th>
+                                            <th>Email</th>
+                                            <th>Message</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+                                        <tr>
+                                            <td class="col-3">
+                                                <div class="d-flex align-items-center">
+                                                    <div class="avatar avatar-md">
+                                                        <img src="assets/images/faces/5.jpg">
+                                                    </div>
+                                                    <p class="font-bold ms-3 mb-0">Nelson</p>
+                                                </div>
+                                            </td>
+
+                                            <td class="col-auto">
+                                                <p class=" mb-0">nelson@nelson</p>
+                                            </td>
+
+                                            <td class="col-auto">
+                                                <p class=" mb-0">Excellent Frero</p>
+                                            </td>
+
+                                        </tr>
+                                        
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Message login -->
+
+
+
+                </div>
+                
+            </div>
+        </div>
+
+
+
+      
+        
+            <div class="card">
+                <div class="card-header">
+                    <h4>Visite</h4>
+                </div>
+                <div class="card-body">
+                    <div id="chart-visitors-profile"></div>
+                </div>
+            </div>
+
+
+        </div>
+    </section>
+</div>
+
+            <footer>
+                <div class="footer clearfix mb-0 text-muted">
+                    <div class="float-start">
+                        <p>2021 &copy; Nelson Boda</p>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    
+<script src="assets/vendors/apexcharts/apexcharts.js"></script>
+<script src="assets/js/pages/dashboard.js"></script>
+
+    <script src="assets/js/mazer.js"></script>
+</body>
+
+</html>
---
+++ log/admin/dashboard/dist/sidebar.php
@@ -0,0 +1,146 @@
+<div class="sidebar-menu">
+        <ul class="menu">
+            <li class="sidebar-title"><strong>Menu</strong></li>
+            
+            <li class="sidebar-item ">
+                <a href="index.html" class='sidebar-link'>
+                    <i class="bi bi-grid-fill"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+
+            <!-- Tableau-->
+            
+            <li class="sidebar-title"><strong>Tableaux</strong></li>
+            
+            <li class="sidebar-item  has-sub">
+                <a href="#" class='sidebar-link'>
+                    <i class="bi bi-file-earmark-spreadsheet-fill"></i>
+                    <span>Data</span>
+                </a>
+                <ul class="submenu ">
+
+                    <li class="submenu-item ">
+                        <a href="table-datatable.html">Visiteurs</a>
+                    </li>
+
+                    <li class="submenu-item ">
+                        <a href="table-datatable.html">Professionnel</a>
+                    </li>
+
+                    <li class="submenu-item ">
+                        <a href="table-datatable.html">Hébergement</a>
+                    </li>
+
+                    <li class="submenu-item ">
+                        <a href="table-datatable.html">Conseils</a>
+                    </li>
+
+                </ul>
+            </li>
+
+            <!-- Comptes-->
+
+            <li class="sidebar-title"><strong>Comptes</strong></li>
+            
+            <li class="sidebar-item  has-sub">
+                <a href="#" class='sidebar-link'>
+                    <i class="bi bi-file-earmark-spreadsheet-fill"></i>
+                    <span>Suppression</span>
+                </a>
+                <ul class="submenu ">
+
+                    <li class="submenu-item ">
+                        <a href="auth-register.html">Data</a>
+                    </li>
+
+                </ul>
+            </li>
+
+            <li class="sidebar-item  has-sub">
+                <a href="#" class='sidebar-link'>
+                    <i class="bi bi-file-earmark-spreadsheet-fill"></i>
+                    <span>Enregistrement</span>
+                </a>
+                <ul class="submenu ">
+
+                    <li class="submenu-item ">
+                        <a href="auth-register.html">Utilisateurs</a>
+                    </li>
+
+                    <li class="submenu-item ">
+                        <a href="auth-register.html">Administrateur</a>
+                    </li>
+                </ul>
+            </li>
+
+            <!-- Projets-->
+
+            <li class="sidebar-title"><strong>Projets</strong></li>
+            
+            <li class="sidebar-item  has-sub">
+                <a href="#" class='sidebar-link'>
+                    <i class="bi bi-file-earmark-spreadsheet-fill"></i>
+                    <span>Personnel</span>
+                </a>
+
+                <ul class="submenu ">
+                    <li class="submenu-item ">
+                        <a href="auth-register.html">PortFolio</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="sidebar-item  has-sub">
+                <a href="#" class='sidebar-link'>
+                    <i class="bi bi-file-earmark-spreadsheet-fill"></i>
+                    <span>Professionnel</span>
+                </a>
+
+                <ul class="submenu ">
+                    <li class="submenu-item ">
+                        <a href="auth-register.html">Serveurs de Prod</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="sidebar-item  has-sub">
+                <a href="#" class='sidebar-link'>
+                    <i class="bi bi-file-earmark-spreadsheet-fill"></i>
+                    <span>Externes</span>
+                </a>
+
+                <ul class="submenu ">
+                    <li class="submenu-item ">
+                        <a href="auth-register.html">Projet X</a>
+                    </li>
+                </ul>
+            </li>
+
+            <!-- Demandes-->
+
+            <li class="sidebar-title"><strong>Projets</strong></li>
+
+            <li class="sidebar-item  ">
+                <a href="form-layout.html" class='sidebar-link'>
+                    <i class="bi bi-file-earmark-medical-fill"></i>
+                    <span>Hébergement</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item  ">
+                <a href="form-layout.html" class='sidebar-link'>
+                    <i class="bi bi-file-earmark-medical-fill"></i>
+                    <span>Aides</span>
+                </a>
+            </li>
+
+<br>
+
+
+            <div class="toggler">
+                <h4><a href="../../../../logout.php" class="sidebar-hide d-xl-none d-block">Déconnexion</a></h4>
+            </div>
+
+        </ul>
+    </div>
